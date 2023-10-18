107edf642f923239b0b7bf61e5ac99f846d55c27
chriswillsflannery <chris.flannery@koneksahealth.com>
2023-10-18 17:30:06 -0400

    use axios with header

--- src/util/postJsonToStudies.ts
+++ src/util/postJsonToStudies.ts
@@ -1,22 +1,12 @@
-export const postJsonToStudies = async (jsonStrings: string) => {
-  const endpointUrl = 'http://localhost:3030/organizations/c822245c-d893-454c-81d0-509432b4561e/studies/9199e421-87c0-48ec-94b8-63821f9a7695/scanned-assessments'; // Replace with your actual endpoint URL
+import axios from "axios";
 
-  return await fetch(endpointUrl, {
-    method: 'POST',
-    body: JSON.stringify(jsonStrings),
-  })
-    .then(response => {
-      if (!response.ok) {
-        throw new Error('Network response was not ok');
-      }
-      return response.json(); // Parse the response as JSON
-    })
-    .then(responseData => {
-      // Handle the response data here
-      console.log('Response Data:', responseData);
-    })
-    .catch(error => {
-      // Handle any errors that occurred during the fetch.
-      console.error('Error:', error);
-    });
+export const postJsonToStudies = async (jsonStrings: string) => {
+  const endpointUrl = 'http://localhost:3888/organizations/c822245c-d893-454c-81d0-509432b4561e/studies/9199e421-87c0-48ec-94b8-63821f9a7695/scanned-assessments'; // Replace with your actual endpoint URL
+
+  const res = await axios.post(endpointUrl, JSON.stringify(jsonStrings), {
+    headers: { "Content-Type": "application/json" }
+  });
+
+  console.log('res after post to JSON', res);
+
 }
